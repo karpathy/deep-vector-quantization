756a936ae3adb0e762fbb34ad463c4a1f0006ec4
Andrej Karpathy <andrej.karpathy@gmail.com>
2021-02-27 15:25:32 -0800

    reshuffling the code around a bit as a step 1 of conforming a bit better to the pytorch lightning project template and style guide

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 
 # deep vector quantization
 
-Random experiments with VQVAE and friends, i.e. autoencoder models that pass through discrete latent variable bottlenecks, which are then easy to subsequently plug into existing infrastructure for modeling sequences of discrete variables (GPT and friends). I didn't get a chance to make the code pretty and consume and propagate all the proper args etc. - currently this is very much not a "pass arguments in watch it work" kind of repo, this is "read the entire code and hack things inline" code.
+Experiments with VQVAEs, i.e. autoencoders with categorical latent variable bottlenecks, which are then easy to subsequently plug into existing infrastructure for modeling sequences of discrete variables (GPT and friends). In a semi-rough state with magic number in the code, still in process of cleaning up...
 
 DeepMind's [VQVAE](https://github.com/deepmind/sonnet/blob/v2/examples/vqvae_example.ipynb)
 
-Should train out of the box as `python train_vqvae.py --gpus 1 --vq_flavor vqvae`.
+Should train out of the box as `cd dvq; python vqvae.py --gpus 1 --vq_flavor vqvae`.
 
 I am able to get what I think are expected results on CIFAR-10 using VQVAE (judging by reconstruction loss achieved). However I had to resort to a data-driven intialization scheme with k-means, which the sonnet repo does not use, potentially due to more careful model initialization treatment. When I do not use data-driven init the training exhibits catastrophic index collapse.
 
 Jang et al. [Gumbel Softmax](https://arxiv.org/abs/1611.01144) version, also the version used in [DALL-E](https://openai.com/blog/dall-e/) allegedly, though we have not seen the details yet.
 
-Should train out of the box as `python train_vqvae.py --gpus 1 --vq_flavor gumbel`.
+Should train out of the box as `cd dvq; python vqvae.py --gpus 1 --vq_flavor gumbel`.
 
 Trains and converges to slightly higher reconstruction loss, but tuning the scale of the kl divergence loss and the temperature decay rate and the version of gumbel (soft/hard) has so far proved a little bit finicky. Also the whole thing trains much slower. Requires a bit more thorough hyperparameter search than a few one-off guesses.
--- dvq/vqvae.py
+++ dvq/vqvae.py
@@ -3,14 +3,19 @@ Defines the full (PyTorch Lightning module) VQVAE, which incorporates an
 encoder, decoder and a quantize layer in the middle for the discrete bottleneck.
 """
 
+import os
+from argparse import ArgumentParser
+
 import torch
 from torch import nn, einsum
 import torch.nn.functional as F
 
 import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
 
-from dvq.model.deepmind_enc_dec import DeepMindEncoder, DeepMindDecoder
-from dvq.model.quantize import VQVAEQuantize, GumbelQuantize
+from data.cifar10 import CIFAR10Data
+from model.deepmind_enc_dec import DeepMindEncoder, DeepMindDecoder
+from model.quantize import VQVAEQuantize, GumbelQuantize
 
 # -----------------------------------------------------------------------------
 
@@ -108,3 +113,39 @@ class VQVAE(pl.LightningModule):
         optimizer = torch.optim.AdamW(optim_groups, lr=3e-4, weight_decay=1e-5)
 
         return optimizer
+
+
+def cli_main():
+    pl.seed_everything(1337)
+
+    parser = ArgumentParser()
+    # model related
+    parser.add_argument("--vq_flavor", type=str, default='vqvae', choices=['vqvae', 'gumbel'])
+    # data related
+    parser.add_argument("--data_dir", type=str, default='/apcv/users/akarpathy/cifar10')
+    parser.add_argument("--batch_size", type=int, default=128)
+    parser.add_argument("--num_workers", type=int, default=8)
+    parser = pl.Trainer.add_argparse_args(parser)
+    args = parser.parse_args()
+
+    data = CIFAR10Data(args)
+    model = VQVAE(args)
+
+    checkpoint_callback = ModelCheckpoint(monitor='val_recon_error', mode='min')
+
+    class DecayTemperature(pl.Callback):
+        def on_train_epoch_start(self, trainer, pl_module):
+            e = trainer.current_epoch
+            e0,e1 = 0, 30
+            t0,t1 = 1.0, 0.1
+            alpha = max(0, min(1, (e - e0) / (e1 - e0)))
+            t = alpha * t1 + (1 - alpha) * t0 # probably should be exponential instead
+            print("epoch %d setting temperature of model's quantizer to %f" % (e, t))
+            pl_module.quantizer.temperature = t
+
+    trainer = pl.Trainer.from_argparse_args(args, callbacks=[checkpoint_callback, DecayTemperature()])
+
+    trainer.fit(model, data)
+
+if __name__ == "__main__":
+    cli_main()
